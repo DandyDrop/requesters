5c424137776fa1ba53e1d02efb3dee1f2443541f
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-02-11 12:23:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 data = {os.environ.get("PASS") : os.environ.get("IDENTIFICATOR")}
 
-@app.route('/', methods= ['HEAD'])
-def handle():
-  requests.post(os.environ.get("LINK"), data=data)
-  return ""
+@app.route('/', methods=['HEAD'])
+def send():
+    requests.post(os.environ.get('LINK'), data=data)
+    return ""
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
     
