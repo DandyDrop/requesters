fc5458edbd442e8c3bd31bd251ebc070f063be7d
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-03-18 17:51:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ data = {os.environ.get("PASS"): os.environ.get("IDENTIFICATOR")}
 @app.before_request
 def send():
     try:
-        requests.post(os.environ.get('LINK'), data=data, timeout=10)
+        requests.post(os.environ.get('LINK'), data=data, timeout=3)
     except requests.exceptions.ReadTimeout:
         pass
     return ""
